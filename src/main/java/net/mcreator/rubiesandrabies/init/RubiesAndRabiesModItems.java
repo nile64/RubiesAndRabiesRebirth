7211daaf8a8882bdd4c78324c1a4d5fab0733e86
Poqqerss <golde@DESKTOP-4JLAAEQ>
2026-09-20 21:59:29 -0400

    test

--- src/main/java/net/mcreator/rubiesandrabies/init/RubiesAndRabiesModItems.java
+++ src/main/java/net/mcreator/rubiesandrabies/init/RubiesAndRabiesModItems.java
@@ -14,6 +14,7 @@ import net.minecraft.world.item.BlockItem;
 
 import net.mcreator.rubiesandrabies.item.OldPebbleItem;
 import net.mcreator.rubiesandrabies.item.NinteenohfourItem;
+import net.mcreator.rubiesandrabies.item.ItemseItem;
 import net.mcreator.rubiesandrabies.item.ArchfirechargeItem;
 import net.mcreator.rubiesandrabies.RubiesAndRabiesMod;
 
@@ -40,6 +41,7 @@ public class RubiesAndRabiesModItems {
 	public static final DeferredItem<Item> OLD_TILES_STAIRS;
 	public static final DeferredItem<Item> ARCHBLAZE_SPAWN_EGG;
 	public static final DeferredItem<Item> ARCHFIRECHARGE;
+	public static final DeferredItem<Item> ITEMSE;
 	static {
 		NINTEENOHFOURDIMENSION = register("ninteenohfourdimension", NinteenohfourItem::new);
 		OLD_GRASS = block(RubiesAndRabiesModBlocks.OLD_GRASS);
@@ -60,6 +62,7 @@ public class RubiesAndRabiesModItems {
 		OLD_TILES_STAIRS = block(RubiesAndRabiesModBlocks.OLD_TILES_STAIRS);
 		ARCHBLAZE_SPAWN_EGG = register("archblaze_spawn_egg", properties -> new SpawnEggItem(properties.spawnEgg(RubiesAndRabiesModEntities.ARCHBLAZE.get())));
 		ARCHFIRECHARGE = register("archfirecharge", ArchfirechargeItem::new);
+		ITEMSE = register("itemse", ItemseItem::new);
 	}
 
 	// Start of user code block custom items
