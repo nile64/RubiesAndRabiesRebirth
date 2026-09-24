bb9634e1a0d5998e17c845f5adcce980329b2a99
Poqqerss <goldedgookie@gmail.com>
2026-09-24 00:41:18 -0400

    infernal enchant final

--- src/main/java/net/mcreator/rubiesandrabies/procedures/InfernoOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/rubiesandrabies/procedures/InfernoOnEffectActiveTickProcedure.java
@@ -1,5 +1,10 @@
 package net.mcreator.rubiesandrabies.procedures;
 
+import net.neoforged.neoforge.event.tick.EntityTickEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
@@ -10,16 +15,28 @@ import net.minecraft.core.particles.ParticleTypes;
 
 import net.mcreator.rubiesandrabies.init.RubiesAndRabiesModMobEffects;
 
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
 public class InfernoOnEffectActiveTickProcedure {
+	@SubscribeEvent
+	public static void onEntityTick(EntityTickEvent.Pre event) {
+		execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity());
+	}
+
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
 		if (entity instanceof LivingEntity _livEnt0 && _livEnt0.hasEffect(RubiesAndRabiesModMobEffects.INFERNO)) {
-			if ((entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(RubiesAndRabiesModMobEffects.INFERNO) ? _livEnt.getEffect(RubiesAndRabiesModMobEffects.INFERNO).getDuration() : 0) % 20 == 0) {
+			if ((entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(RubiesAndRabiesModMobEffects.INFERNO) ? _livEnt.getEffect(RubiesAndRabiesModMobEffects.INFERNO).getDuration() : 0) % 20 == 10) {
 				{
 					Entity _ent = entity;
 					if (_ent.level() instanceof ServerLevel _serverLevel) {
-						_ent.hurtServer(_serverLevel, new DamageSource(world.holderOrThrow(DamageTypes.ON_FIRE)), 1);
+						_ent.hurtServer(_serverLevel, new DamageSource(world.holderOrThrow(DamageTypes.MAGIC)), 1);
 					}
 				}
 				if (world instanceof ServerLevel _level)
